87f01e98f06376704568930a4d477f94edc15409
Haruomi-Sakamoto <hal808932@gmail.com>
2025-02-23 14:46:05 +0900

    git test

--- auv25_ros/auv25_ros/pingsonner.py
+++ auv25_ros/auv25_ros/pingsonner.py
@@ -4,13 +4,13 @@ from std_msgs.msg import Float32
 
 class PingsonnerNode(Node):
     def __init__(self):
-        super().__init__('pingsonner_node')  # ノード名を 'pingsonner_node' に変更
+        super().__init__('pingsonner_node')
         self.publisher_ = self.create_publisher(Float32, 'sensor_data', 10)
         self.timer = self.create_timer(1.0, self.timer_callback)
 
     def timer_callback(self):
         # センサーからデータを取得する処理をここに追加
-        sensor_data = 10.5  # 仮のデータ
+        sensor_data = 10.5
         self.publisher_.publish(Float32(data=sensor_data))
         self.get_logger().info(f'Sensor data: {sensor_data}')
 
