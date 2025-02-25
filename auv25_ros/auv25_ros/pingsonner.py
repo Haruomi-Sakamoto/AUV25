1064ae9afa25a2e656b6e6c76f39589fccc3caa2
Haruomi-Sakamoto <hal808932@gmail.com>
2025-02-25 17:59:57 +0900

    Update ping

--- auv25_ros/auv25_ros/pingsonner.py
+++ auv25_ros/auv25_ros/pingsonner.py
@@ -2,9 +2,13 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
 
+from brping import Ping1D
+
 class PingsonnerNode(Node):
     def __init__(self):
         super().__init__('pingsonner_node')
+        #self.myping = Ping1D()
+        #self.myping.connect_serial("/dev/ttyUSB0", 115200)
         self.publisher_ = self.create_publisher(Float32, 'sensor_data', 10)
         self.timer = self.create_timer(1.0, self.timer_callback)
 
@@ -14,6 +18,10 @@ class PingsonnerNode(Node):
 
         self.publisher_.publish(Float32(data=sensor_data))
         self.get_logger().info(f'Sensor data: {sensor_data}')
+        
+    def Sensing(self):
+        pass
+        
 
 def main(args=None):
     rclpy.init(args=args)
