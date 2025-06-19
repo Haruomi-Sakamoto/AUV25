11e6c27c82278829f13c089d33567105c4c26d66
Haruomi-Sakamoto <hal808932@gmail.com>
2025-06-19 16:35:06 +0900

    update j2tw

--- auv25_ros/auv25_ros/config.py
+++ auv25_ros/auv25_ros/config.py
@@ -15,7 +15,7 @@ class MPU6050Config:
         self.gyro_scale_modifier = 131.0     # ±250 °/s
         self.gravity = 9.80665               # m/s²
 
-                # 加速度共分散の対角成分（m/s^2 の分散）
+        # 加速度共分散の対角成分（m/s^2 の分散）
         self.accel_cov_diag = [0.1, 0.1, 0.1]
 
         # 角速度共分散の対角成分（rad/s の分散）
