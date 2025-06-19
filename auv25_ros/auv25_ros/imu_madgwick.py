a19ee1eed5995311304a7dbab4e6b2810388a9bd
Haruomi-Sakamoto <hal808932@gmail.com>
2025-06-19 17:41:36 +0900

    fix imu_madgwick

--- auv25_ros/auv25_ros/imu_madgwick.py
+++ auv25_ros/auv25_ros/imu_madgwick.py
@@ -28,7 +28,7 @@ class ImuMadgwickNode(Node):
         dt = (current_time - self.last_time).nanoseconds * 1e-9
         self.last_time = current_time
 
-        acc_g = np.array([
+        acc = np.array([
             msg.linear_acceleration.x, 
             msg.linear_acceleration.y,
             msg.linear_acceleration.z
