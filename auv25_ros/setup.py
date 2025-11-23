0d1f494769a9080266bb7f1637a65275e56546aa
Haruomi-Sakamoto <hal808932@gmail.com>
2025-11-23 17:54:49 +0900

    Fix config

--- auv25_ros/setup.py
+++ auv25_ros/setup.py
@@ -1,24 +1,23 @@
-from setuptools import setup, find_packages
-import os
-from glob import glob
+from setuptools import setup
 
 package_name = 'auv25_ros'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        ('share/' + package_name + '/launch', ['launch/auv25_launch.py', 'launch/auv25_remotelaunch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='user',
-    maintainer_email='user@example.com',
-    description='AUV ROS2 package',
-    license='Apache License 2.0',
-    tests_require=['pytest'],
+    maintainer='haruomi',
+    maintainer_email='haruomi@todo.todo',
+    description='AUV sensor and thruster control package',
+    license='Apache-2.0',
     entry_points={
         'console_scripts': [
             'pingsonner = auv25_ros.pingsonner:main',
