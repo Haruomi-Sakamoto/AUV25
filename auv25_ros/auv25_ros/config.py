b30716a7d57e14d39dbd3c4a3282f9f71744394a
Haruomi-Sakamoto <hal808932@gmail.com>
2025-11-24 12:40:20 +0900

    Fix config

--- auv25_ros/auv25_ros/config.py
+++ auv25_ros/auv25_ros/config.py
@@ -51,7 +51,7 @@ class PCA9685Config:
         self.pwmfreq = 50
 
         self.camera_channel = 3
-        self.thruster_channel = [15,11,7,5,9,13]
+        self.thruster_channel = [15,13,9,5,7,11]
         #self.thruster_channel = [13,7,9,5,11,15]
         self.pwm_neutral_us = 1500   # stop
         self.pwm_range_us   = 400
@@ -62,7 +62,7 @@ class PCA9685Config:
 
 
         """
-        15 1
+        15 9
         9 9
-        11 15
+        13 15
         """
